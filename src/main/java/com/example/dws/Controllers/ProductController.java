3de15f22dde06eb507f176b09f1b2838d7181034
adrianAD1 <a.alvarezd.2021@alumnos.urjc.es>
2025-02-18 10:53:27 +0100

    cambios

--- src/main/java/com/example/dws/Controllers/ProductController.java
+++ src/main/java/com/example/dws/Controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.dws.Controllers;
 
 
+import com.example.dws.Entities.Product;
 import com.example.dws.Entities.Shop;
 import com.example.dws.Repositories.ProductRepository;
 import com.example.dws.Repositories.ShopRepository;
@@ -19,18 +20,11 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping("/products/{productID}")
-    public String getShopById(@PathVariable("productId") long shopId, Model model) {
-        Shop shop = shopRepository.findById(shopId);
-        if (shop != null) {
-            model.addAttribute("shop", shop);
-            // Mostrar los productos de la tienda y sus atributos
-            System.out.println("Productos de la tienda " + shop.getShopName() + ": ");
-            shop.getProducts().forEach((productId, product) -> {
-                System.out.println("Producto ID: " + product.getProductId());
-                System.out.println("Nombre: " + product.getProductName());
-                System.out.println("Precio: " + product.getProductPrize());
-            });
-            return "showShop"; // Vista que muestra los detalles de la tienda
+    public String getProductById(@PathVariable("productID") long productID, Model model) {
+        Product product = productRepository.findById(productID);
+        if (product != null) {
+            model.addAttribute("product", product);
+            return "showProduct"; // Vista que muestra los detalles de la tienda
         } else {
             return "error"; // Vista de error si no se encuentra la tienda
         }
