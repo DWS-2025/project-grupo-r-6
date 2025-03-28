92c29ee9720c71c7c58d2e9291432b4fdaf7649d
adrianAD1 <a.alvarezd.2021@alumnos.urjc.es>
2025-03-28 13:36:28 +0100

    Product Controller y Service

---
+++ src/main/java/com/example/dws/Service/ProductService.java
@@ -0,0 +1,33 @@
+package com.example.dws.Service;
+
+import com.example.dws.Entities.Product;
+import com.example.dws.Entities.Shop;
+import com.example.dws.Repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Optional<Product> findById(long id) {
+        return productRepository.findById(id);
+    }
+    public void saveShopInProduct(Product product, Shop shop) {
+        if (!product.getShops().contains(shop)) {
+            product.getShops().add(shop);
+        }
+        productRepository.save(product);
+    }
+    public void deleteById(long id){
+        productRepository.deleteById(id);
+    }
+    public void update(Product product, String name, double price) {
+        product.setProductName(name);
+        product.setProductPrize(price);
+        productRepository.save(product);
+    }
+}
