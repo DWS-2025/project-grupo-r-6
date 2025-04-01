a62741b7a2ac97ea6df903deb74bc19793a8fec5
adrianAD1 <a.alvarezd.2021@alumnos.urjc.es>
2025-04-01 12:44:28 +0200

    Create ShopDTO.java

---
+++ src/main/java/com/example/dws/DTOs/ShopDTO.java
@@ -0,0 +1,18 @@
+package com.example.dws.DTOs;
+
+import com.example.dws.Entities.Comment;
+import com.example.dws.Entities.Product;
+
+import java.sql.Blob;
+import java.util.List;
+
+record ShopDTO(
+        Long id,
+        String shopName,
+        List<Product> products,
+        List<Comment> comments,
+        String imageName,
+        Blob imageFile
+) {
+
+}
