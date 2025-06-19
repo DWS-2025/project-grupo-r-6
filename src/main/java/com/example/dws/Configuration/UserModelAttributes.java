d0bef160a9fa1a8efd8a182cd353a6c2c5487aa3
agarcia <a.garciace.2021@alumnos.urjc.es>
2025-06-19 14:25:22 +0200

    Cambio a HTTPS

---
+++ src/main/java/com/example/dws/Configuration/UserModelAttributes.java
@@ -0,0 +1,29 @@
+package com.example.dws.Configuration;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
+
+@ControllerAdvice(basePackages = "es.codeurjc.daw.library.controller.web")
+public class UserModelAttributes {
+    
+	@ModelAttribute
+	public void addAttributes(Model model, HttpServletRequest request) {
+
+		Principal principal = request.getUserPrincipal();
+
+		if (principal != null) {
+
+			model.addAttribute("logged", true);
+			model.addAttribute("userName", principal.getName());
+			model.addAttribute("admin", request.isUserInRole("ADMIN"));
+
+		} else {
+			model.addAttribute("logged", false);
+		}
+	}
+
+}
